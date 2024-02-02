615a3ee8792dac5092e02afed4d55b8bc57ea44a
torsteinelv <torstein@thorsland.net>
2024-02-02 14:34:09 +0100

    Update script.py

--- deye-solarmanpy/script.py
+++ deye-solarmanpy/script.py
@@ -3,7 +3,6 @@ import os
 import sys
 import logging
 from pysolarmanv5 import PySolarmanV5
-from datetime import datetime
 import time
 import json
 
@@ -33,7 +32,7 @@ if ip_address is None or serial_number is None:
     logger.error("Configuration values 'ip_address' or 'serial_number' are missing.")
     sys.exit(1)
 
-print(f"{token}: {ip_address}, {serial_number}")
+logger.info(f"Configuration values: Token={token}, IP={ip_address}, Serial Number={serial_number}")
 
 # Home Assistant REST API URL
 url = "http://supervisor/core/api"
@@ -68,15 +67,73 @@ while True:
             time.sleep(60)
             continue
 
-        # Convert state2 to an integer
-        try:
-            state2 = int(state2)
-        except ValueError:
-            logger.error("Error converting state2 to an integer.")
-            time.sleep(60)
-            continue
+        # Log the new states
+        if state != prev_state:
+            logger.info(f"State changed! New state: {state}")
 
-        # Rest of your script...
+            # Convert state to a float
+            state_float = float(state)
+
+            # Calculate voltage from state
+            def input_to_voltage(input_val):
+                voltage = (input_val / 100) * 1680 + 4200
+                voltage = round(voltage)
+                return voltage
+
+            input_val = round(state_float)
+            voltage = input_to_voltage(input_val)
+
+            if input_val < 20 or input_val > 90:
+                logger.warning("Input value is outside of the valid range (0-100)")
+                continue
+
+            # Call the main function to send an update to the inverter for state1
+            modbus = PySolarmanV5(ip_address, serial_number, port=8899, mb_slave_id=1, verbose=False)
+
+            # Read the current value from the inverter
+            read = modbus.read_holding_registers(register_addr=262, quantity=1)
+            read_battery_amps = modbus.read_holding_registers(register_addr=210, quantity=1)
+            logger.debug(f"Current voltage on the inverter: {read[0]}")
+
+            # If the current value matches the desired value, skip the write
+            if read[0] == voltage:
+                logger.info(f"Skipping write, same value ({read[0]} voltage)")
+                prev_state = state
+            # Otherwise, write the new value
+            else:
+                write = modbus.write_multiple_holding_registers(register_addr=262, values=[voltage])
+                if write == 1:
+                    logger.info(f"Write to the inverter successful, new voltage: {voltage}")
+                    prev_state = state
+                else:
+                    logger.warning("Write to the inverter failed!")
+
+        state2 = int(state2)
+        if state2 != prev_state2:
+            logger.info(f"State2 changed! New state: {state2} / {prev_state2}")
+            state2 = int(state2)
+
+            if state2 > 40:
+                logger.warning("Input value2 is outside of the valid range (0-40)")
+                continue
+
+            # Call the main function to send an update to the inverter for state2
+            modbus = PySolarmanV5(ip_address, serial_number, port=8899, mb_slave_id=1, verbose=False)
+
+            # Read the current value from the inverter
+            read = modbus.read_holding_registers(register_addr=210, quantity=1)
+
+            if read[0] == state2:
+                logger.info(f"Skipping write, same value ({read[0]} amps {state2})")
+                prev_state2 = state2
+            # Otherwise, write the new value
+            else:
+                write = modbus.write_multiple_holding_registers(register_addr=210, values=[state2])
+                if write == 1:
+                    logger.info(f"Write to the inverter successful, new amp now: {state2}")
+                    prev_state2 = state2
+                else:
+                    logger.warning("Write to the inverter failed!")
 
     except Exception as e:
         logger.error(f"An error occurred: {e}")
